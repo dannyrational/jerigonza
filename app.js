ebf4d8abd2bba13e776ebb616f3f0b5c6250265e
Daniel Rodas <drodas6232@gmail.com>
2021-08-10 01:18:35 +0100

    3: Working on Styling

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let inputField = document.querySelector("#word-input");
-const vowels = ["a", "e", "i", "o", "u", "á", "é", "í ", "ó", "ú"];
-let resultArray = [];
-let outputText = document.querySelector("#output");
 let translateBtn = document.querySelector("#translate-btn");
 
-translateBtn.addEventListener("click", function translate(e) {
-  e.preventDefault();
+translateBtn.addEventListener("click", () => {
+  const vowels = ["a", "e", "i", "o", "u", "á", "é", "í ", "ó", "ú"];
+  let resultArray = [];
+  let outputText = document.querySelector("#output");
+
   let inputText = inputField.value.toLowerCase().split("");
   outputText.innerText = resultArray.splice(0, resultArray.length)
   for (let i = 0; i < inputText.length; i++) {
@@ -18,31 +18,3 @@ translateBtn.addEventListener("click", function translate(e) {
   }
   outputText.innerText = resultArray.join("");
 });
-
-
-
-
-// function translation(e) {
-//   e.preventDefault();
-//   let inputTextField = inputField.value.split("");
-
-//   outputText.innerText = resultArray.splice(0, resultArray.length)
-
-//   for (let letter = 0; letter < inputTextField.length; letter++) {
-//     for (let vowel = 0; vowel < vowels.length; vowel++) {
-//       if (inputTextField[letter] === vowels[vowel]) {
-//         if (inputTextField[letter] === "e") {
-//           resultArray.push("ee");
-//         } else if (inputTextField[letter] === "u") {
-//           resultArray.push("uu");
-//         } else if (inputTextField[letter] === " ") {
-//           resultArray.push(" ");
-//         } else {
-//           resultArray.push(inputTextField[letter]);
-//         }
-//       }
-//     }
-//   }
-
-//   outputText.innerText = resultArray.join("").toUpperCase();
-// }
